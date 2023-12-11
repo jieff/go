7abcade939dcb5ba5f43696c849deb8d8649685b
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2023-12-11 17:54:26 -0300

    Feat: create deleteAcoount html

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ func main() {
 
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/policy", policy)
-		v1.GET("/terms", terms)
+		v1.GET("/privacy", privacy)
+		v1.GET("/delete-account", deleteAccount)
 	}
 
 	router.Run(":3007")
 
 }
 
-func policy(c *gin.Context) {
-	htmlContent, err := os.ReadFile("policy.html")
+func privacy(c *gin.Context) {
+	htmlContent, err := os.ReadFile("privacy.html")
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Erro ao carregar o arquivo HTML")
 		return
@@ -32,6 +32,14 @@ func policy(c *gin.Context) {
 	c.String(http.StatusOK, string(htmlContent))
 }
 
-func terms(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Terms of use"})
+func deleteAccount(c *gin.Context) {
+	//c.JSON(http.StatusOK, gin.H{"message": "Terms of use"})
+	htmlContent, err := os.ReadFile("deleteAccount.html")
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Erro ao carregar o arquivo HTML")
+		return
+	}
+
+	c.Header("Content-Type", "text/html")
+	c.String(http.StatusOK, string(htmlContent))
 }
