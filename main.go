4af2e88f1b7685841f5db4cc514b7aeca0d2fc7e
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2024-03-15 21:23:55 +0000

    update add support_dlist

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 
 	router := gin.Default()
 
+	// Middleware para permitir CORS
+	router.Use(cors.Default())
+
 	v1 := router.Group("/v1")
 	{
 		v1.GET("/privacy", privacy)
 		v1.GET("/delete-account", deleteAccount)
+		v1.GET("/support_dlist", support_dlist)
 	}
 
 	router.Run(":3007")
@@ -44,6 +50,18 @@ func privacy(c *gin.Context) {
 func deleteAccount(c *gin.Context) {
 	//c.JSON(http.StatusOK, gin.H{"message": "Terms of use"})
 	htmlContent, err := os.ReadFile("deleteAccount.html")
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Erro ao carregar o arquivo HTML")
+		return
+	}
+
+	c.Header("Content-Type", "text/html")
+	c.String(http.StatusOK, string(htmlContent))
+}
+
+func support_dlist(c *gin.Context) {
+	//c.JSON(http.StatusOK, gin.H{"message": "Terms of use"})
+	htmlContent, err := os.ReadFile("support_dlist.html")
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Erro ao carregar o arquivo HTML")
 		return
